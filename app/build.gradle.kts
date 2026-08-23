17565776232626b4a6fc7f51c2409d8b907bf07d
jeanpiersebast28 <jeanpiersebast28@outlook.com>
2026-08-23 17:42:50 -0500

    version 2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.tymusic"
         minSdk = 24
         targetSdk = 37
-        versionCode = 4
-        versionName = "2.3"
+        versionCode = 5
+        versionName = "2.4"
     }
 
     buildTypes {
